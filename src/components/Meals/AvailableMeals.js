ceec758f8faebbbc2b20e9fdb39dc5baea92f89c
Sohaib Irfan <tihor.personal@gmail.com>
2022-08-18 10:19:52 +0530

    Added card component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,4 @@
+import Card from "../UI/Card";
 import styles from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -32,7 +33,9 @@ const AvailableMeals = () => {
 
   return (
     <section className={styles.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
