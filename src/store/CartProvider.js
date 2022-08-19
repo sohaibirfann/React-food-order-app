3b4a1d026ed801bfbbb9ae04d987388af71c0590
Sohaib Irfan <tihor.personal@gmail.com>
2022-08-19 11:11:37 +0530

    updated cart reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -10,6 +10,11 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
     const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
   return defaultCartState;
 };
