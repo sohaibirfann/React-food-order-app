81e4a301b673bbb8e89dd3e779a6ec4eb775620e
Sohaib Irfan <tihor.personal@gmail.com>
2022-08-26 10:02:36 +0530

    updated components and styling

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,8 +1,12 @@
 import classes from "./Checkout.module.css";
 
-const Checkout = () => {
+const Checkout = (props) => {
+  const confirmHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form>
+    <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" />
@@ -19,7 +23,12 @@ const Checkout = () => {
         <label htmlFor="city">City</label>
         <input type="text" id="city" />
       </div>
-      <button>Confirm</button>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
